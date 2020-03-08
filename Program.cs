78cd36bb44e96e2f7b9a472471a8b6ba225f3e1b
Vahe99 <61380709+Vahe99@users.noreply.github.com>
2020-03-08 16:46:25 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 
-namespace ConsoleApp4
-{
+namespace ConsoleApp4._2
+{    
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+
             int[] x = new int[10];
             x[0] = 24;
             x[1] = 15;
@@ -18,30 +19,56 @@ namespace ConsoleApp4
             x[8] = 77;
             x[9] = 12;
             int a = 0;
-            for(int i = 0; i < x.Length; i++ )
+            for (int i = 0; i < x.Length; i++)
             {
-                a += x[i];                
+                a += x[i];
             }
-            Console.WriteLine(s);
-            
-            int[] z = new int[5];
-            Console.WriteLine("Enter the first number");
-            z[0] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            z[1] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the third number");
-            z[2] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the fourth number");
-            z[3] = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the fifth number");
-            z[4] = Convert.ToInt32(Console.ReadLine());
-            int s = 0;
+            Console.WriteLine("x = " + a);
 
-            for(int d = 0; d < z.Length; d++)
-            {
-                s += z[d];
+            Console.WriteLine();
+
+            int[] q = new int[10];
+            q[0] = 24;
+            q[1] = 15;
+            q[2] = 2;
+            q[3] = 4;
+            q[4] = 85;
+            q[5] = 29;
+            q[6] = 61;
+            q[7] = 46;
+            q[8] = 77;
+            q[9] = 12;
+            int p = 0;
+            int z = 0;
+            while (z < q.Length)
+            {              
+                p += q[z];
+                z++;
+                Console.WriteLine("p = " + p);
             }
-            Console.WriteLine("Adding up all the numbers you wrote exatly - " + s);
+
+            Console.WriteLine();
+
+            int[] d = new int[10];
+            d[0] = 24;
+            d[1] = 15;
+            d[2] = 2;
+            d[3] = 4;
+            d[4] = 85;
+            d[5] = 29;
+            d[6] = 61;
+            d[7] = 46;
+            d[8] = 77;
+            d[9] = 12;
+            int j = 0;
+            int h = 0;
+            do
+            {
+                j += d[h];
+                h++;
+                Console.WriteLine("j = " + j);
+            } while (h < d.Length) ;
+
 
         }
     }
